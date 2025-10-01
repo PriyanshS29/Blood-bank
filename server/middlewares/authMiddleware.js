7e153b32c29e6c0d6e37abdd0be493422d1999f4
Anurag <anuragrai211004@gmail.com>
2025-10-01 22:45:16 +0530

    fix: Correct JWT_SECRET case in auth middleware

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -20,7 +20,7 @@ const jwt = require("jsonwebtoken");
 module.exports = function (req, res, next) {
   try {
     const token = req.header("authorization").replace("Bearer ", "");
-    const decryptedData = jwt.verify(token, process.env.jwt_secret);
+    const decryptedData = jwt.verify(token, process.env.JWT_SECRET);
     req.body.userId = decryptedData.userId;
     next();
   } catch (error) {
